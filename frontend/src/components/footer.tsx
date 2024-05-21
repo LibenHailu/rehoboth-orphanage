5eb33fb4b34bbbc9800bd48d8b68b1ccaacb9460
Liben Hailu <libenhailu04@gmail.com>
2024-05-21 20:07:15 +0300

    Update footer UI

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -22,8 +22,8 @@ const Footer = () => {
     return (
         <footer className="mx-auto w-full flex items-center gap-6 border-t py-6 pb-28 sm:pb-6 border-foreground/25 bg-background/95 px-3 shadow-md supports-[backdrop-filter]:bg-background/60 shadow-md supports-[backdrop-filter]:bg-background/60 supports-[backdrop-filter]:bg-clip-padding supports-[backdrop-filter]:backdrop-blur sm:justify-between sm:rounded-none sm:px-3">
             {/* <div className="container flex flex-col items-center justify-between space-y-4 text-center lg:flex-row lg:space-y-0 lg:text-left"> */}
-            <div className="grid grid-cols-5 gap-x-6 mx-auto max-w-6xl">
-                <div className="space-y-4 sm:col-span-1">
+            <div className="container grid grid-cols-5 gap-x-6 mx-auto max-w-6xl gap-y-4">
+                <div className="space-y-2 hidden lg:block lg:col-span-1">
                     <Image
                         priority={false}
                         className="hidden sm:block"
@@ -33,38 +33,37 @@ const Footer = () => {
                         height="150"
                     />
                 </div>
-                <div className="space-y-4 sm:col-span-1">
+                <div className="space-y-2 col-span-5 sm:col-span-2 lg:col-span-1 text-center sm:text-start">
                     <h5 className="text-xl font-bold">Get Involved</h5>
-                    <p>
+                    <p className="text-sm">
                         Get involved in transforming the lives of orphans through care and education!
                     </p>
                     <Button
                     >
                         Support Us
                     </Button>
-                    {/* </ul> */}
                 </div>
-                <div className="space-y-4 sm:col-span-1">
+                <div className="space-y-2 col-span-5 sm:col-span-1 text-center sm:text-start">
                     <h5 className="text-xl font-bold">Pages</h5>
-                    <ul className="space-y-4">
+                    <ul className="space-y-2">
                         {
-                            flattenedLinks.map(link => <li key={link.href}><Link href={link.href}>{link.title}</Link></li>)
+                            flattenedLinks.map(link => <li key={link.href}><Link href={link.href} className="text-sm">{link.title}</Link></li>)
                         }
                     </ul>
                 </div>
-                <div className="space-y-4 sm:col-span-2">
+                <div className="space-y-2 col-span-5 sm:col-span-2 text-center sm:text-start">
                     <h5 className="text-xl font-bold">Contact Us</h5>
-                    <ul className="space-y-4">
-                        <li className="flex flex-wrap gap-x-2">
-                            <MapPin className="w-5 h-5 text-muted-foreground pt-2 flex-shrink-0" />
+                    <ul className="space-y-2">
+                        <li className="flex flex-wrap items-center text-sm gap-x-2 justify-center sm:justify-start">
+                            <MapPin className="w-6 h-6 text-muted-foreground pt-2" />
                             {defaultUser?.address}
                         </li>
-                        <li className="flex flex-wrap gap-x-2">
-                            <Phone className="w-5 h-5 text-muted-foreground pt-2 flex-shrink-0" />
+                        <li className="flex flex-wrap items-center text-sm gap-x-2 justify-center sm:justify-start">
+                            <Phone className="w-6 h-6 text-muted-foreground pt-2" />
                             {defaultUser?.phone}
                         </li>
-                        <li className="flex flex-wrap gap-x-2">
-                            <Mail className="w-5 h-5 text-muted-foreground pt-2 flex-shrink-0" />
+                        <li className="flex flex-wrap items-center text-sm gap-x-2 justify-center sm:justify-start">
+                            <Mail className="w-6 h-6 text-muted-foreground pt-2" />
                             {defaultUser?.email}
                         </li>
                     </ul>
